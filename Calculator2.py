14b7d473067764f7afd643376af9210ca6e7731d
HaitexAF <93163938+HaitexAF@users.noreply.github.com>
2022-05-14 02:49:44 -0700

    Add files via upload

---
+++ Calculator2.py
@@ -0,0 +1,25 @@
+while True:
+    Operation = input("send your Operation... ")
+    if "+" in Operation:
+        j = Operation.index("+")
+        x1 = int(Operation[:j])
+        y1 = int(Operation[j+1:])
+        print(f"{x1} + {y1} = {x1 + y1}")
+    elif "-" in Operation:
+        m = Operation.index("-")
+        x2 = int(Operation[0:m])
+        y2 = int(Operation[m+1:])
+        print(f"{x2} - {y2} = {x2 - y2}")
+        
+    elif "*" in Operation:
+        z = Operation.index("*")
+        x3 = int(Operation[0:z])
+        y3 = int(Operation[z+1:])
+        print(f"{x3} * {y3} = {x3 * y3}")
+    elif "/" in Operation:
+        t = Operation.index("/")
+        x4 = int(Operation[0:t])
+        y4 = int(Operation[t+1:])
+        print(f"{x4} / {y4} = {x4 / y4}")
+    else:
+        raise ValueError
