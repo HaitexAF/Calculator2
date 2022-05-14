4b6d016260f898f3b412f66937b176c64b929013
HaitexAF <93163938+HaitexAF@users.noreply.github.com>
2022-05-14 03:26:27 -0700

    Update Calculator2.py

--- Calculator2.py
+++ Calculator2.py
@@ -2,19 +2,19 @@ while True:
     Operation = input("send your Operation... ")
     if "+" in Operation:
         j = Operation.index("+")
-        x1 = int(Operation[:j])
-        y1 = int(Operation[j+1:])
+        x1 = float(Operation[:j])
+        y1 = float(Operation[j+1:])
         print(f"{x1} + {y1} = {x1 + y1}")
     elif "-" in Operation:
         m = Operation.index("-")
-        x2 = int(Operation[0:m])
-        y2 = int(Operation[m+1:])
+        x2 = float(Operation[0:m])
+        y2 = float(Operation[m+1:])
         print(f"{x2} - {y2} = {x2 - y2}")
         
     elif "*" in Operation:
         z = Operation.index("*")
-        x3 = int(Operation[0:z])
-        y3 = int(Operation[z+1:])
+        x3 = float(Operation[0:z])
+        y3 = float(Operation[z+1:])
         print(f"{x3} * {y3} = {x3 * y3}")
     elif "/" in Operation:
         t = Operation.index("/")
